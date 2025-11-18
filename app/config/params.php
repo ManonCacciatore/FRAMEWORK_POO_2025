273fb5fd209dacdb13637e9e65afb9046bb5ee01
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-11-18 09:39:20 +0100

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$title = "Books & Authors";
+$content = "";
+
+// PARAMETRES DE CONNEXION
+define('DB_NAME', "book_hunter_2025");
+define('DB_HOST', "localhost:3307");
+define('DB_USER', "root");
+define('DB_PASSWORD', "root");
