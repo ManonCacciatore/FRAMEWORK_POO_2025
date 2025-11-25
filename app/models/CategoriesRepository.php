82132c8bc3bec42961d33f0c20d3adef7a667fc7
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-11-25 09:50:07 +0100

    Affichage des categories dans le aside

---
+++ app/models/CategoriesRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use \PDO, \Core\DB;
+
+abstract class CategoriesRepository
+{
+
+    public static function findAll(): array
+    {
+        $sql = "SELECT *
+            FROM categories
+            ORDER BY name ASC;";
+        $rs = DB::getConnexion()->query($sql);
+        return $rs->fetchAll(PDO::FETCH_CLASS, Category::class);
+    }
+}
